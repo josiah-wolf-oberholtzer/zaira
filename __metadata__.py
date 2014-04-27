df55213b017de4e2fec6e6be8fec097c0e3c9b1d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-04-27 18:32:11 -0400

    Updated root metadata.

--- __metadata__.py
+++ __metadata__.py
@@ -3,5 +3,8 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('forces_tagline', 'for Ensemble Mosaik'),
+    ('paper_dimensions', '11x17'),
     ('title', 'Invisible Cities: Zaira'),
+    ('year_of_completion', 2014),
     ])
