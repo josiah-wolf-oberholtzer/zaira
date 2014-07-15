5b087acf40878a573caa586d0c0242e8a4f1028e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-15 17:32:46 -0400

    Adjusted names in segment definitions.

--- segments/segment_02/definition.py
+++ segments/segment_02/definition.py
@@ -13,6 +13,6 @@ segment_maker.set_duration_in_seconds(
         ),
     )
 segment_maker.set_rehearsal_mark('B')
-segment_maker.set_score_template(zaira.makers.score_template)
+segment_maker.set_score_template(zaira.makers.ZairaScoreTemplate)
 segment_maker.set_tempo(zaira.materials.tempi[0])
 segment_maker.set_time_signatures(zaira.materials.time_signatures)
