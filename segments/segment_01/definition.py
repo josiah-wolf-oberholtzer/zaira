0a6822b08926be317a6e49165bc928aafbc7264c
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-15 16:27:31 -0400

    Added segment stubs.

--- segments/segment_01/definition.py
+++ segments/segment_01/definition.py
@@ -1,2 +1,11 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+import consort
+import zaira
+
+
+segment_maker = consort.makers.ConsortSegmentMaker()
+
+segment_maker.set_score_template(zaira.makers.score_template)
+segment_maker.set_tempo(zaira.materials.tempi[0])
+segment_maker.set_duration_in_seconds(3)
+segment_maker.set_time_signatures(zaira.materials.time_signatures)
