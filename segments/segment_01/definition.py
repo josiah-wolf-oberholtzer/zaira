77b0443e7987ad6a970db2ca53a1240024b1ee1f
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-15 17:13:06 -0400

    Stubbed out more segment maker settings.

--- segments/segment_01/definition.py
+++ segments/segment_01/definition.py
@@ -3,9 +3,16 @@ import consort
 import zaira
 
 
+segment_index = 0
 segment_maker = consort.makers.ConsortSegmentMaker()
 
+segment_maker.set_duration_in_seconds(
+    zaira.materials.proportions.get_segment_duration_in_seconds(
+        segment_index,
+        zaira.materials.total_duration_in_seconds,
+        ),
+    )
+segment_maker.set_rehearsal_mark('A')
 segment_maker.set_score_template(zaira.makers.score_template)
 segment_maker.set_tempo(zaira.materials.tempi[0])
-segment_maker.set_duration_in_seconds(3)
 segment_maker.set_time_signatures(zaira.materials.time_signatures)
--- segments/segment_04/definition.py
+++ segments/segment_04/definition.py
@@ -1,2 +1,18 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+import consort
+import zaira
+
+
+segment_index = 3
+segment_maker = consort.makers.ConsortSegmentMaker()
+
+segment_maker.set_duration_in_seconds(
+    zaira.materials.proportions.get_segment_duration_in_seconds(
+        segment_index,
+        zaira.materials.total_duration_in_seconds,
+        ),
+    )
+segment_maker.set_rehearsal_mark('D')
+segment_maker.set_score_template(zaira.makers.score_template)
+segment_maker.set_tempo(zaira.materials.tempi[0])
+segment_maker.set_time_signatures(zaira.materials.time_signatures)
