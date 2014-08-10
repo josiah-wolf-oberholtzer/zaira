219f35a95965c5194c691249a0598b61ab6e8108
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-10 20:03:39 +0200

    Added demo music construct to segment 10.

--- segments/segment_10/definition.py
+++ segments/segment_10/definition.py
@@ -1,11 +1,14 @@
 # -*- encoding: utf-8 -*-
+from abjad import *
+from experimental import *
 import zaira
+import consort
 
 
 segment_index = 9
 segment_maker = zaira.makers.ZairaSegmentMaker(
     rehearsal_mark='J',
-    tempo=zaira.materials.tempi[0],
+    tempo=zaira.materials.tempi[1],
     )
 
 segment_maker.set_duration_in_seconds(
@@ -13,4 +16,12 @@ segment_maker.set_duration_in_seconds(
         segment_index,
         zaira.materials.total_duration_in_seconds,
         ),
+    )
+
+segment_maker.add_construct(
+    music_specifier=consort.makers.MusicSpecifier(),
+    timespan_maker=zaira.materials.sparse_timespan_maker,
+    voice_identifier=(
+        '.+ Voice',
+        )
     )
