15c3e5c4e3adefafd19d2615a3845ec77dda119e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-11 15:50:47 +0200

    Added sustained_timespan_maker material.

---
+++ materials/sustained_timespan_maker/definition.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import consort
+
+
+sustained_timespan_maker = consort.makers.TimespanMaker(
+    can_split=True,
+    minimum_duration=durationtools.Duration(1, 8),
+    playing_durations=(
+        durationtools.Duration(1, 4),
+        ),
+    playing_groupings=(1,),
+    repeat=True,
+    silence_durations=(
+        durationtools.Duration(1, 4),
+        ),
+    step_anchor=Right,
+    synchronize_groupings=False,
+    synchronize_step=False,
+    )
