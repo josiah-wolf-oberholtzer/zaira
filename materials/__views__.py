a8d60030460075659e1945f1f14184ac604f3bd8
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-12 18:02:20 +0200

    Added rhythm maker materials view.

---
+++ materials/__views__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from scoremanager import idetools
+
+
+view_inventory=idetools.ViewInventory(
+    [
+        (
+            'rhythm makers',
+            idetools.View(
+                ["'rhythm maker' in :ds:"]
+                ),
+            ),
+        ]
+    )
