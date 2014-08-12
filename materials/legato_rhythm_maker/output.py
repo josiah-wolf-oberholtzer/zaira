392dd4df72709a00770532db8b5b52d28c995dfe
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-12 17:04:38 +0200

    Wrote legato rhythm maker material.

---
+++ materials/legato_rhythm_maker/output.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+legato_rhythm_maker = rhythmmakertools.TaleaRhythmMaker(
+    talea=rhythmmakertools.Talea(
+        counts=(3, 2, 1, 1, 1, 4, 3, 1, 1, 1, 1, 5, 2, 4),
+        denominator=8,
+        ),
+    split_divisions_by_counts=(4, 3, 6, 5),
+    extra_counts_per_division=(0, 0, 1, 2, 0, 1),
+    burnish_specifier=rhythmmakertools.BurnishSpecifier(
+        burnish_divisions=False,
+        burnish_output=True,
+        lefts=(-1,),
+        left_lengths=(1, 0),
+        ),
+    tie_split_notes=True,
+    )
