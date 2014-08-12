d5bf9249dddaf7323190a754f53ef44e25cda178
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-12 16:37:35 +0200

    Wrote sustained rhythm maker material.

---
+++ materials/sustained_rhythm_maker/output.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+sustained_rhythm_maker = rhythmmakertools.NoteRhythmMaker(
+    beam_specifier=rhythmmakertools.BeamSpecifier(
+        beam_each_division=False,
+        beam_divisions_together=False,
+        ),
+    tie_specifier=rhythmmakertools.TieSpecifier(
+        tie_across_divisions=(True, True, False),
+        ),
+    )
