deeb31e5ab0e46036357b27cf89e214d2cf81218
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-19 00:24:21 -0400

    Added clefs to score template.

--- makers/ZairaScoreTemplate.py
+++ makers/ZairaScoreTemplate.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 import collections
 from abjad.tools import abctools
+from abjad.tools import indicatortools
 from abjad.tools import instrumenttools
 from abjad.tools import scoretools
+from abjad.tools.topleveltools import attach
 from consort import makers
 
 
@@ -31,20 +33,26 @@ class ZairaScoreTemplate(abctools.AbjadValueObject):
         flute, label = manager.make_single_wind_performer(
             instrument=instrumenttools.Flute(),
             )
+        flute_voice = flute[0][0]
+        attach(indicatortools.Clef('treble'), flute_voice)
         labels.append(label)
-        self._voice_name_abbreviations['flute'] = flute[0][0].name
+        self._voice_name_abbreviations['flute'] = flute_voice.name
 
         oboe, label = manager.make_single_wind_performer(
             instrument=instrumenttools.Oboe(),
             )
+        oboe_voice = oboe[0][0]
+        attach(indicatortools.Clef('treble'), oboe_voice)
         labels.append(label)
-        self._voice_name_abbreviations['oboe'] = oboe[0][0].name
+        self._voice_name_abbreviations['oboe'] = oboe_voice.name
 
         clarinet, label = manager.make_single_wind_performer(
             instrument=instrumenttools.ClarinetInEFlat(),
             )
+        clarinet_voice = clarinet[0][0]
+        attach(indicatortools.Clef('treble'), clarinet_voice)
         labels.append(label)
-        self._voice_name_abbreviations['clarinet'] = clarinet[0][0].name
+        self._voice_name_abbreviations['clarinet'] = clarinet_voice.name
 
         winds = scoretools.StaffGroup(
             [
@@ -94,10 +102,14 @@ class ZairaScoreTemplate(abctools.AbjadValueObject):
         piano, label = manager.make_single_piano_performer(
             instrument=instrumenttools.Piano(),
             )
+        upper_voice = piano[0][0]
+        lower_voice = piano[2][0]
+        attach(indicatortools.Clef('treble'), upper_voice)
+        attach(indicatortools.Clef('bass'), lower_voice)
         labels.append(label)
-        self._voice_name_abbreviations['piano_rh'] = piano[0][0].name
+        self._voice_name_abbreviations['piano_rh'] = upper_voice.name
         self._voice_name_abbreviations['piano_dynamics'] = piano[1].name
-        self._voice_name_abbreviations['piano_lh'] = piano[2][0].name
+        self._voice_name_abbreviations['piano_lh'] = lower_voice.name
         self._voice_name_abbreviations['piano_pedals'] = piano[3].name
 
         ### STRINGS ###
@@ -106,22 +118,28 @@ class ZairaScoreTemplate(abctools.AbjadValueObject):
             instrument=instrumenttools.Violin(),
             split=False,
             )
+        violin_voice = violin[0][0]
+        attach(indicatortools.Clef('treble'), violin_voice)
         labels.append(label)
-        self._voice_name_abbreviations['violin'] = violin[0][0].name
+        self._voice_name_abbreviations['violin'] = violin_voice.name
 
         viola, label = manager.make_single_string_performer(
             instrument=instrumenttools.Viola(),
             split=False,
             )
+        viola_voice = viola[0][0]
+        attach(indicatortools.Clef('alto'), viola_voice)
         labels.append(label)
-        self._voice_name_abbreviations['viola'] = viola[0][0].name
+        self._voice_name_abbreviations['viola'] = viola_voice.name
 
         cello, label = manager.make_single_string_performer(
             instrument=instrumenttools.Cello(),
             split=False,
             )
+        cello_voice = cello[0][0]
+        attach(indicatortools.Clef('bass'), cello_voice)
         labels.append(label)
-        self._voice_name_abbreviations['cello'] = cello[0][0].name
+        self._voice_name_abbreviations['cello'] = cello_voice.name
 
         strings = scoretools.StaffGroup(
             [
