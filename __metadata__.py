39f94cff86a09dead0d995013b5d24bb0e2d9f4d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-22 00:10:01 -0400

    Updated score metadata.

--- __metadata__.py
+++ __metadata__.py
@@ -6,8 +6,10 @@ metadata = collections.OrderedDict([
     ('catalog_number', 'WN-2014-1'),
     ('composer', 'Oberholtzer'),
     ('forces_tagline', 'for Ensemble Mosaik'),
+    ('locale', 'Jamaica Plain'),
     ('paper_dimensions', '11x17'),
     ('price', '\\euro 66 / \\$84'),
+    ('time_period', ('June 2014', 'September 2014')),
     ('title', 'Invisible Cities (i): Zaira'),
     ('year', 2014),
     ])
