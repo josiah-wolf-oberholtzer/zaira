37334f1a3fc3d1b86f4bda2c4e932f3baccca578
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-09 13:57:55 -0700

    Updated output material.

--- zaira/materials/tempi/output.py
+++ zaira/materials/tempi/output.py
@@ -6,15 +6,15 @@ from abjad.tools import indicatortools
 tempi = indicatortools.TempoInventory(
     [
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 4),
+            reference_duration=durationtools.Duration(1, 4),
             units_per_minute=72,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 4),
+            reference_duration=durationtools.Duration(1, 4),
             units_per_minute=48,
             ),
         indicatortools.Tempo(
-            duration=durationtools.Duration(1, 4),
+            reference_duration=durationtools.Duration(1, 4),
             units_per_minute=108,
             ),
         ]
