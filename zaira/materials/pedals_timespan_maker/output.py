eddb32993e08a6e74c31a62bdbbc9d4ccc77682f
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-12-27 22:51:53 -0500

    Modernizing material packages.

--- zaira/materials/pedals_timespan_maker/output.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-import consort
-
-
-pedals_timespan_maker = consort.tools.DependentTimespanMaker(
-    labels=('pedaled',),
-    voice_names=('Piano Upper Voice', 'Piano Lower Voice'),
-    )
--- zaira/materials/total_duration_in_seconds/output.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-
-total_duration_in_seconds = 480
