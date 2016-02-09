89e484bf79d7d528db5e45ec520c666b38aca96e
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2016-02-08 21:25:22 -0800

    Revised shaker music specifier.

--- zaira/materials/shaker_music_specifier/definition.py
+++ zaira/materials/shaker_music_specifier/definition.py
@@ -1,18 +1,20 @@
 # -*- encoding: utf-8 -*-
 from abjad import Markup
 from abjad.tools import indicatortools
+from abjad.tools import rhythmmakertools
+from abjad.tools import patterntools
 from abjad.tools import spannertools
 from abjad.tools import selectortools
 import consort
-from zaira.materials.background_dynamic_attachment_expression.definition \
-    import background_dynamic_attachment_expression
-from zaira.materials.undergrowth_rhythm_maker.definition \
-    import undergrowth_rhythm_maker
 
 
 shaker_music_specifier = consort.MusicSpecifier(
     attachment_handler=consort.AttachmentHandler(
-        dynamic_expression=background_dynamic_attachment_expression,
+        dynamic_expression=consort.AttachmentExpression(
+            attachments=indicatortools.Dynamic('p'),
+            selector=selectortools.Selector()
+                .by_leaves()[0]
+            ),
         clef_spanner=consort.ClefSpanner('percussion'),
         staff_lines_spanner=spannertools.StaffLinesSpanner(
             lines=(4, -4),
@@ -22,17 +24,20 @@ shaker_music_specifier = consort.MusicSpecifier(
                 }
             ),
         staccato=consort.AttachmentExpression(
-            attachments=indicatortools.Articulation('.'),
-            selector=selectortools.Selector(
-                ).by_logical_tie(pitched=True
-                ).by_duration('<', (1, 8)
-                ).by_length(1)
+            attachments=[[
+                indicatortools.LaissezVibrer(),
+                indicatortools.Articulation('accent'),
+                ]],
+            selector=selectortools.Selector()
+                .by_logical_tie(pitched=True)
+                .by_duration('<=', (1, 8))
+                .by_length(1)
             ),
         stem_tremolo_spanner=consort.AttachmentExpression(
             attachments=spannertools.StemTremoloSpanner(),
-            selector=selectortools.Selector(
-                ).by_logical_tie(pitched=True
-                ).by_duration('>', (1, 16))
+            selector=selectortools.Selector()
+                .by_logical_tie(pitched=True)
+                .by_duration('>', (1, 4))
             ),
         text_spanner=consort.AttachmentExpression(
             attachments=consort.ComplexTextSpanner(
@@ -47,5 +52,28 @@ shaker_music_specifier = consort.MusicSpecifier(
     pitch_handler=consort.AbsolutePitchHandler(
         pitches_are_nonsemantic=True,
         ),
-    rhythm_maker=undergrowth_rhythm_maker,
+    rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
+        beam_specifier=rhythmmakertools.BeamSpecifier(
+            beam_each_division=False,
+            beam_divisions_together=False,
+            ),
+        division_masks=[
+            rhythmmakertools.SustainMask(
+                pattern=patterntools.Pattern(
+                    indices=[1],
+                    period=3,
+                    ),
+                ),
+            ],
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            fill_with_notes=False,
+            outer_divisions_only=False,
+            prefix_talea=(1,),
+            prefix_counts=(1,),
+            talea_denominator=16,
+            ),
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            tie_across_divisions=True,
+            )
+        ),
     )
